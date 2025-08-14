63b0d5c8d51a8900f264f702a8e6eeef2455f36a
N. Seymour-Smith <nrs23@sussex.ac.uk>
2025-08-14 16:10:18 +0100

    feat: Index and Register view showing register entries

--- stationers/urls.py
+++ stationers/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "stationers"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:register_id>/", views.register_detail, name="detail")
 ]
--- stationers/views.py
+++ stationers/views.py
@@ -1,5 +1,17 @@
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from .models import Register, Entry
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the stationer's register index.")
+    register_list = Register.objects.order_by("-register_start_date")
+
+    context = {"register_list": register_list}
+    return render(request, "stationers/index.html", context)
+
+
+def register_detail(request, register_id):
+    register = get_object_or_404(Register, pk=register_id)
+    register_entries = Entry.objects.filter(register=register_id)
+    return render(request, "stationers/register_detail.html",
+                  {"register": register, "entries": register_entries})
