743e63049999a96fe0176840c8be586bd2692aad
N. Seymour-Smith <nrs23@sussex.ac.uk>
2025-08-15 15:03:32 +0100

    feat: Separate list of entries into a separate page

--- stationers/urls.py
+++ stationers/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 app_name = "stationers"
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("<int:register_id>/", views.register_detail, name="detail")
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:register_id>/", views.register_detail, name="detail"),
+    path("<int:register_id>_all/", views.entry_list, name="entry_list")
 ]
--- stationers/views.py
+++ stationers/views.py
@@ -1,17 +1,26 @@
 from django.shortcuts import get_object_or_404, render
+from django.views import generic
 
 from .models import Register, Entry
 
 
-def index(request):
-    register_list = Register.objects.order_by("-register_start_date")
+class IndexView(generic.ListView):
+    template_name = "stationers/index.html"
+    context_object_name = "register_list"
 
-    context = {"register_list": register_list}
-    return render(request, "stationers/index.html", context)
+    def get_queryset(self):
+        return Register.objects.order_by("-register_start_date")
 
 
 def register_detail(request, register_id):
     register = get_object_or_404(Register, pk=register_id)
+    n_register_entries = len(Entry.objects.filter(register=register_id))
+    context = {"register": register, "entries": n_register_entries}
+    return render(request, "stationers/register_detail.html", context)
+
+
+def entry_list(request, register_id):
+    register = get_object_or_404(Register, pk=register_id)
     register_entries = Entry.objects.filter(register=register_id)
-    return render(request, "stationers/register_detail.html",
-                  {"register": register, "entries": register_entries})
+    context = {"register": register, "entries": register_entries}
+    return render(request, "stationers/entry_list.html", context)
