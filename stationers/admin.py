34bef6e04abb672cf8ddbc0b7c53cf1427156cb4
N. Seymour-Smith <nrs23@sussex.ac.uk>
2025-08-15 17:01:16 +0100

    feat: add a library entry model for data from libraries

--- stationers/admin.py
+++ stationers/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Register, Entry
-
+from .models import Register, Entry, LibraryEntry
 
 admin.site.register(Register)
 admin.site.register(Entry)
+admin.site.register(LibraryEntry)
--- stationers/models.py
+++ stationers/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class Register(models.Model):
@@ -20,6 +21,27 @@ class Entry(models.Model):
     entry_volumes = models.CharField(max_length=100, blank=True)
     entry_edition = models.CharField(max_length=100, blank=True)
     register_page = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.entry_author}: {self.entry_title}"
+
+
+class LibraryEntry(models.Model):
+    class Library(models.TextChoices):
+        BODLEIAN_LIBRARY = "BOL", _("Bodleian Library")
+        BRITISH_LIBRARY = "BRL", _("British Library")
+        CAMBRIDGE_LIBRARY = "CAL", _("Cambridge Library")
+        SCOTLAND_LIBRARY = "SCL", _("National Library of Scotland")
+        TRINITY_LIBRARY = "TRL", _("Trinity College Dublin Library")
+
+    source_library = models.CharField(max_length=3,
+                                      choices=Library,
+                                      default=Library.BRITISH_LIBRARY)
+    entry_date = models.DateField("date of entry")
+    entry_author = models.CharField(max_length=100)
+    entry_title = models.CharField(max_length=500)
+    entry_volumes = models.CharField(max_length=100, blank=True)
+    entry_edition = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
         return f"{self.entry_author}: {self.entry_title}"
