0484d5d5cc7e7cd6d66c5e2dd071645738f6bc6e
N. Seymour-Smith <nrs23@sussex.ac.uk>
2025-08-26 11:29:51 +0100

    feat (admin): add confirm status, entry no's and other conveniences

--- stationers/admin.py
+++ stationers/admin.py
@@ -14,32 +14,47 @@ class MatchInline(admin.TabularInline):
 
 class RegisterAdmin(admin.ModelAdmin):
     inlines = [RegisterEntryInline]
-    list_display = ["name", "pages", "file"]
+    list_display = ["name", "pages", "file", "_entry_count"]
+
+    def _entry_count(self, obj):
+        return obj.registerentry_set.count()
+
+    _entry_count.short_description = "Entry Count"
 
 
 class RegisterEntryAdmin(admin.ModelAdmin):
     inlines = [MatchInline]
-    list_display = ["title", "author", "date", "register"]
+    list_display = [
+        "title", "author", "date", "register", "confirmed_match",
+        "_match_count"
+    ]
     list_filter = ["register", "confirmed_match"]
     search_fields = ["title", "author"]
+
+    def _match_count(self, obj):
+        return obj.matchcandidate_set.count()
+
+    _match_count.short_description = "Match Candidate Count"
 
 
 class LibraryEntryAdmin(admin.ModelAdmin):
     inlines = [MatchInline]
-    list_display = [
-        "title", "author", "date", "source_library"
-    ]
+    list_display = ["title", "author", "date", "source_library"]
     list_filter = ["source_library"]
     search_fields = ["title", "author"]
 
 
 class MatchAdmin(admin.ModelAdmin):
     list_display = [
-        "register_entry__title", "register_entry__author",
-        "register_entry__register", "library_entry__title",
-        "library_entry__author", "library_entry__source_library"
+        "register_entry__title", "library_entry__title",
+        "register_entry__author", "library_entry__author",
+        "register_entry__register", "library_entry__source_library",
+        "confirmed_match"
     ]
-    list_filter = ["register_entry__register", "library_entry__source_library"]
+    list_filter = [
+        "register_entry__register", "library_entry__source_library",
+        "confirmed_match"
+    ]
     search_fields = [
         "register_entry__title", "register_entry__author",
         "library_entry__title", "library_entry__author"
